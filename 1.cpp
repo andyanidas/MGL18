19b15a41b600e00bca8fa0325b1f7a11ccf19490
Lkhagva-erdene <anidas.com@gmail.com>
2021-03-11 14:20:50 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"Hello World"<<endl;
+	cout<<"Hello Mongolia"<<endl;
+	cout<<"Hello World"<<endl;
+	cout<<"Hello Mongolia"<<endl;
+	cout<<"Hello World"<<endl;
+	cout<<"Hello Mongolia"<<endl;
+	
+	
+	return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//variable declare //huvisagch taniulah
+	int x,y,nemeh,hasah; //int ->integer buyu buhel too //value buyu utga
+	
+	//utga ogch baina
+	x = 15;
+	y = 20;
+	nemeh = x + y;
+	hasah = x - y;
+	
+	cout<<"x + y = "<<nemeh<<endl;
+	cout<<"x - y = "<<hasah;
+	
+	return 0;
+}
+//ene bol comment 
+//comment n code nd yamar negen noloo uzuulehgui
